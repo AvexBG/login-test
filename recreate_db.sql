36805407db4f6ee8da8e6498c7e14b9d2fed7978
AvexBG <xevabg@gmail.com>
2023-01-17 00:36:30 -0500

    Update recreate_db.sql

---
+++ recreate_db.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA sql_login;
+
+USE sql_login;
+
+CREATE TABLE users (
+	id int(11) AUTO_INCREMENT PRIMARY KEY,
+	username varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    password text NOT NULL,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL
+);
